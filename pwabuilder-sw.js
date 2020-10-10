8f5dfb65064f81c0f5d7c43d4fe76766fa7fc540
Yurii Tsimbal <sellsteam13@mail.ru>
2020-10-10 16:41:56 +0300

    commit

---
+++ pwabuilder-sw.js
@@ -0,0 +1,29 @@
+// This is the service worker with the Cache-first network
+
+if ('function' === typeof importScripts)
+
+{
+    const CACHE = "pwabuilder-offline";
+
+    importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+    self.addEventListener("message", (event) => {
+        if (event.data && event.data.type === "SKIP_WAITING") {
+            self.skipWaiting();
+        }
+    });
+
+    workbox.loadModule('workbox-cacheable-response');
+    workbox.loadModule('workbox-range-requests');
+
+    workbox.routing.registerRoute(
+        /.*\.mp4/,
+        new workbox.strategies.CacheFirst({
+            cacheName: CACHE,
+            plugins: [
+                new workbox.cacheableResponse.CacheableResponsePlugin({ statuses: [200] }),
+                new workbox.rangeRequests.RangeRequestsPlugin(),
+            ],
+        }),
+    );
+}
